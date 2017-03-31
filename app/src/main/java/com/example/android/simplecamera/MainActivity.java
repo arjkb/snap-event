df856e956b5c4b4bf297b0129fb6f132b547af7a
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-03-31 17:30:46 -0600

    calling the camera app using intent succesfully

--- app/src/main/java/com/example/android/simplecamera/MainActivity.java
+++ app/src/main/java/com/example/android/simplecamera/MainActivity.java
@@ -24,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                dispatchTakePictureIntent();
             }
         });
     }
