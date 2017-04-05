f38b32c6b9a8ef31f62b108ecfb8c2d87482496d
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-04 19:42:45 -0600

    partially defined getView()

--- app/src/main/java/com/example/android/snapevent/GridViewAdapter.java
+++ app/src/main/java/com/example/android/snapevent/GridViewAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.util.List;
@@ -38,6 +40,18 @@ public class GridViewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null)    {
+            // if it's not recycled, recycle some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageBitmap();
         return null;
     }
 }
