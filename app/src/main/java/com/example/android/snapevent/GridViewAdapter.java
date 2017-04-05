834b1de8872ce99ebd167491c97be533c7aaab7e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-04 23:09:07 -0600

    Modifications to the way the gridview is populated.
    Added Log statements for debugging.
    Tweaked imageview parameters.
    Tweaked bitmap scaling.

--- app/src/main/java/com/example/android/snapevent/GridViewAdapter.java
+++ app/src/main/java/com/example/android/snapevent/GridViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.snapevent;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -20,46 +21,55 @@ public class GridViewAdapter extends BaseAdapter {
     private Context mContext;
     private List<File> fileNames;
 
+    String TAG = "GRID_VIEW";
+
     public GridViewAdapter(Context c, List<File> fileNames)    {
+        Log.v(TAG, " Entering GridViewAdapter()");
         mContext = c;
         this.fileNames = fileNames;
     }
 
     @Override
     public int getCount() {
+        Log.v(TAG, " Inside getCount() " + fileNames.size());
         return fileNames.size();
     }
 
     @Override
     public Object getItem(int position) {
+        Log.v(TAG, " Inside getItem()");
         return null;
     }
 
     @Override
     public long getItemId(int position) {
+        Log.v(TAG, " Inside getItemId()");
         return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Log.v(TAG, " Inside getView()");
+
         ImageView imageView;
         if (convertView == null)    {
             // if it's not recycled, recycle some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            imageView.setPadding(2, 2, 2, 2);
         } else {
             imageView = (ImageView) convertView;
         }
 
         imageView.setImageBitmap(getImageBitmap(imageView, position));
-        return null;
+        return imageView;
     }
 
     private Bitmap getImageBitmap(ImageView imageView, int position)   {
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
+        imageView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        int targetW = imageView.getMeasuredWidth();
+        int targetH = imageView.getMeasuredHeight();
 
         // get the dimensions of the bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
@@ -68,8 +78,10 @@ public class GridViewAdapter extends BaseAdapter {
         int photoW = bmOptions.outWidth;
         int photoH = bmOptions.outHeight;
 
+        Log.v(TAG, " >> " + targetH + " " + targetW + " " + photoH + " " + photoW);
+
         // determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        int scaleFactor = Math.min(photoW/100, photoH/100);
 
         // decode the image file into a bitmap sized to fill the view
         bmOptions.inJustDecodeBounds = false;
@@ -77,7 +89,6 @@ public class GridViewAdapter extends BaseAdapter {
         bmOptions.inPurgeable =true;
 
         Bitmap bitmap = BitmapFactory.decodeFile(fileNames.get(position).toString(), bmOptions);
-//        imageView.setImageBitmap(bitmap);
 
         return bitmap;
     }
