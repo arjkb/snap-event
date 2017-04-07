e718dcc6763bb1dcbd4c1ef5501557092fc9c7c0
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-06 18:59:52 -0600

    adding variables to hold the textview and two buttons

--- app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
@@ -3,6 +3,8 @@ package com.example.android.snapevent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by akbfedora on 4/6/17.
@@ -17,6 +19,10 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public Button mButton1;
+        public Button mButton2;
+
         public ViewHolder(View itemView) {
             super(itemView);
         }
