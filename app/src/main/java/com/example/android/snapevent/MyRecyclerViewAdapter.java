aff1de11229f7e8eccc166698b333db2bf8e6142
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-06 18:26:51 -0600

    Implementing a basic RecyclerView class with a static ViewHolder class inside it

--- app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
@@ -1,8 +1,33 @@
 package com.example.android.snapevent;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by akbfedora on 4/6/17.
  */
 
-public class MyRecyclerViewAdapter {
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
