53dc853031f92f7ef1ada726c86ef89ea99d6535
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-18 21:51:03 -0600

    Creating GlideConfiguration class to configure Glide

---
+++ app/src/main/java/com/example/android/snapevent/GlideConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.android.snapevent;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.module.GlideModule;
+
+/**
+ * Created by akbfedora on 4/18/17.
+ */
+
+public class GlideConfiguration implements GlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+
+    }
+}
