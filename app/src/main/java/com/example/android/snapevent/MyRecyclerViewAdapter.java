0d540b572df52d39b2baa882675f5eff9f4f46b7
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-18 22:13:49 -0600

    Removing unused lines of code. Removing now unused getImageBitmap()

--- app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
@@ -24,24 +24,14 @@ import java.util.List;
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
 
-//    String[] sampleDataSet;
     List<File> fileNames;
 
     final String LOG_TAG = "MyRecyclerViewAdapter";
     Context context;
 
-//    public MyRecyclerViewAdapter(String[] dummyText) {
-//        sampleDataSet = dummyText;
-//    }
-
     public MyRecyclerViewAdapter(List<File> imageFileNames) {
         fileNames = imageFileNames;
     }
-
-//    public MyRecyclerViewAdapter(List<File> imageFileNames, String[] dummyText) {
-//        fileNames = imageFileNames;
-//        sampleDataSet = dummyText;
-//    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView mTextView;
@@ -70,14 +60,13 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
         String thisItemFileName = fileNames.get(position).toString();
-//        holder.mTextView.setText(thisItemText);
         holder.mTextView.setText(thisItemFileName);
-//        holder.mImageView.setImageBitmap(getImageBitmap(holder.mImageView, position));
 
         Glide.with(context)
                 .load(fileNames.get(position))
                 .centerCrop()
                 .into(holder.mImageView);
+
         holder.mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,44 +80,10 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
                 Log.v(LOG_TAG, " Clicking button2 at position " + position);
             }
         });
-
     }
 
     @Override
     public int getItemCount() {
         return fileNames.size();
-    }
-
-    private Bitmap getImageBitmap(ImageView imageView, int position)   {
-        imageView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        int targetW = imageView.getMeasuredWidth();
-        int targetH = imageView.getMeasuredHeight();
-
-        // get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = 2;
-        bmOptions.inPurgeable = true;
-
-        Matrix matrix = new Matrix();
-        matrix.postScale(0.5f, 0.5f);
-
-        Bitmap croppedBitmap = null;
-
-        try {
-            Bitmap bitmap = BitmapFactory.decodeFile(fileNames.get(position).toString(), bmOptions);
-            Log.v(LOG_TAG, " Bitmap: " + position);
-            final int x = 0;
-            final int y = bitmap.getHeight()/3;
-            final int height = bitmap.getHeight()/3;
-            final int width = bitmap.getWidth();
-
-            croppedBitmap = Bitmap.createBitmap(bitmap, x, y, width, height, matrix, true);
-        } catch (NullPointerException E)    {
-            Log.e(LOG_TAG, "getImageBitmap(): NULL POINTER EXCEPTION " + E);
-        }
-
-//        return bitmap;
-        return croppedBitmap;
     }
 }
