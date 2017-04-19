aed3654d4bb45b005395fbf8198b89a58ec03e34
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-18 21:51:58 -0600

    setting decode-format to ARGB_8888

--- app/src/main/java/com/example/android/snapevent/GlideConfiguration.java
+++ app/src/main/java/com/example/android/snapevent/GlideConfiguration.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.GlideModule;
 
 /**
@@ -13,7 +14,7 @@ import com.bumptech.glide.module.GlideModule;
 public class GlideConfiguration implements GlideModule {
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
-
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
     }
 
     @Override
