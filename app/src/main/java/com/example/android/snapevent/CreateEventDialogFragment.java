5a05850cb9349d0aa0a3fc74f2952b82e625bd2b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-21 15:59:09 -0600

    defining onCreateDialog() that populates the dialog with title, message, positive, and negative buttons

--- app/src/main/java/com/example/android/snapevent/CreateEventDialogFragment.java
+++ app/src/main/java/com/example/android/snapevent/CreateEventDialogFragment.java
@@ -1,14 +1,21 @@
 package com.example.android.snapevent;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 
 /**
  * Created by akbfedora on 4/21/17.
  */
 
 public class CreateEventDialogFragment extends DialogFragment {
+    final String TAG = "CEDF";
     String dialogMessage = null;
 
     public interface CreateEventDialogListener  {
@@ -31,5 +38,28 @@ public class CreateEventDialogFragment extends DialogFragment {
             throw new ClassCastException(activity.toString()
                             + " must implement CreateEventDialogListener");
         }
+
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setMessage(dialogMessage)
+                .setTitle("Title")
+                .setPositiveButton("Positive Button", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(TAG, "Positive dialog click!");
+                    }
+                })
+                .setNegativeButton("Negative Button", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(TAG, "Negative dialog click!");
+                    }
+                });
+        return builder.create();
     }
 }
