1e6bec88a89393d64536eac23b3d69b42b6a5c03
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-21 15:47:21 -0600

    creating a new class for generating Dialog before event creation

---
+++ app/src/main/java/com/example/android/snapevent/CreateEventDialogFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.snapevent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by akbfedora on 4/21/17.
+ */
+
+public class CreateEventDialogFragment extends DialogFragment {
+}
