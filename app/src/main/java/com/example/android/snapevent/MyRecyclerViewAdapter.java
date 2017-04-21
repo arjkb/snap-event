ff92bb37791f6e10738d346783eb2bcc2841777c
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-21 16:07:41 -0600

    implementing the RecyclerViewButtonClick interface and setting its object in the appropriate onClick() methods

--- app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/snapevent/MyRecyclerViewAdapter.java
@@ -34,6 +34,13 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         fileNames = imageFileNames;
     }
 
+    public interface RecyclerViewButtonClickListener {
+        public void onButton1Click(RecyclerView.ViewHolder vh);
+        public void onButton2Click(RecyclerView.ViewHolder vh);
+    }
+
+    RecyclerViewButtonClickListener recyclerViewButtonClickListener;
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView mTextView;
         private ImageView mImageView;
@@ -59,7 +66,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         String thisItemFileName = fileNames.get(position).toString();
         holder.mTextView.setText(thisItemFileName);
 
@@ -74,6 +81,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
                 String buttonClickMessage = "Clicked button 1 at position " + position;
                 Log.v(LOG_TAG, buttonClickMessage);
                 Toast.makeText(context, buttonClickMessage, Toast.LENGTH_SHORT).show();
+                recyclerViewButtonClickListener.onButton1Click(holder);
             }
         });
 
@@ -83,6 +91,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
                 String buttonClickMessage = "Clicked button 2 at position " + position;
                 Log.v(LOG_TAG, buttonClickMessage);
                 Toast.makeText(context, buttonClickMessage, Toast.LENGTH_SHORT).show();
+                recyclerViewButtonClickListener.onButton2Click(holder);
             }
         });
     }
