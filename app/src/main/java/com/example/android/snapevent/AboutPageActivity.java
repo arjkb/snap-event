9af1b08704fe0a3456872a35a0a83f6ac837350d
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-28 12:21:31 -0600

    creating new class AboutPageActivity and its layout file

---
+++ app/src/main/java/com/example/android/snapevent/AboutPageActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.snapevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.simplecamera.R;
+
+public class AboutPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+    }
+}
