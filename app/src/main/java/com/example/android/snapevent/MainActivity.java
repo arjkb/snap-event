fc4379e3ac851b303e0fd93c89ca2029498fe4b7
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-27 19:28:39 -0600

    created an interface for storing DateType. enum is probably a bad idea in the long run

--- app/src/main/java/com/example/android/snapevent/MainActivity.java
+++ app/src/main/java/com/example/android/snapevent/MainActivity.java
@@ -356,3 +356,9 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 }
+
+interface DateType  {
+    public static final int DAY = 0;
+    public static final int MONTH = 1;
+    public static final int YEAR = 2;
+}
