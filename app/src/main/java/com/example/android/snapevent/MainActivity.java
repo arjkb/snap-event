16d6dd6001b49a0368779edf9b3816503fd6ab7b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-05-18 22:04:12 +0530

    remove unused import statements

--- app/src/main/java/com/example/android/snapevent/MainActivity.java
+++ app/src/main/java/com/example/android/snapevent/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.snapevent;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 
 import android.content.pm.PackageManager;
@@ -15,7 +14,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
